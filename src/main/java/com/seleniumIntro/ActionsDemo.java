1130361bc92b552552458beb46844f643eae6a26
Denys Pugachov <pugachovdenys@gmail.com>
2024-11-25 18:52:06 +0100

    add gitignore

--- src/main/java/com/seleniumIntro/ActionsDemo.java
+++ src/main/java/com/seleniumIntro/ActionsDemo.java
@@ -2,6 +2,6 @@ package com.seleniumIntro;
 
 public class ActionsDemo {
     public static void main(String[] args){
-        System.out.println("test");
+        System.out.println("test2");
     }
 }
