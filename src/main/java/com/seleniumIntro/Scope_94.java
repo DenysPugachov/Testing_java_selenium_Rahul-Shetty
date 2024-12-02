fbf806d02fca2564c8fc7227584fe445ccd6912a
Denys Pugachov <pugachovdenys@gmail.com>
2024-12-02 20:44:50 +0100

    96.add: get all titles of all open tabs

--- src/main/java/com/seleniumIntro/Scope_94.java
+++ src/main/java/com/seleniumIntro/Scope_94.java
@@ -6,12 +6,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.lang.reflect.Array;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class Scope_94 {
     private static int i;
 
-    public static void main(String[] ars) {
+    public static void main(String[] ars) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
@@ -26,16 +31,35 @@ public class Scope_94 {
 
         // Limit WebDriver to first column in first column of footer
         WebElement firstColumnDriver = footerDriver.findElement(By.xpath("//table[contains(@class,'gf-t')] /tbody /tr /td[1] /ul"));
-        int linksInFirstColumnCounter = firstColumnDriver.findElements(By.cssSelector("a")).size();
-        System.out.println("linksInFirstColumnCounter = " + linksInFirstColumnCounter);
+        List<WebElement> linksInFirstColumn = firstColumnDriver.findElements(By.cssSelector(".gf-li > a")); // get elem with class .gf-li with direct parent of <a>
+        System.out.println("linksInFirstColumnCounter = " + linksInFirstColumn.size());
 
         // 4. Click on every link on the column and check if new window is opening
-        for (int i = 0; i < linksInFirstColumnCounter; i++) {
+        for (int i = 0; i < linksInFirstColumn.size(); i++) {
             // Click on link with Command(Control) key(to open link in new window)
             String clickOpenInNewWindow = Keys.chord(Keys.COMMAND, Keys.ENTER);
-            firstColumnDriver.findElements(By.tagName("a")).get(i).sendKeys(clickOpenInNewWindow);
+            firstColumnDriver.findElements(By.cssSelector(".gf-li > a")).get(i).sendKeys(clickOpenInNewWindow);
         }
 
-//        driver.close();
+        //  5. Get title of all open tabs:
+        Set<String> setOfTabs = driver.getWindowHandles(); // get all open tabs
+        Iterator<String> it = setOfTabs.iterator(); // setup iterator to use iterator.next();
+
+        // pause to all tabs are finished loading
+        Thread.sleep(3000);
+
+//        while(it.hasNext()){ // if iterator has next item available
+//            driver.switchTo().window(it.next()); // get next element from iterator
+//            System.out.println("Title:" + driver.getTitle());
+//        }
+
+        // with enhanced for loop => for(type item: collection){
+        for (String tabItem : setOfTabs) { // if iterator has next item available
+            driver.switchTo().window(tabItem); // get next element from iterator
+            System.out.println("Title:" + driver.getTitle());
+        }
+
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
